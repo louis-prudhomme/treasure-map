a1365813131da8fee03158aef41ef21f16596b7d
Louis Prud’homme <louis.prudhomme@hotmail.com>
2021-04-13 19:06:30 +0200

    Fixed adventurers execution order

--- src/main/java/fr/carbonit/engine/GameEngine.java
+++ src/main/java/fr/carbonit/engine/GameEngine.java
@@ -1,19 +1,35 @@
 package fr.carbonit.engine;
 
+import fr.carbonit.exception.ShouldNotHappenException;
 import fr.carbonit.model.actions.EnumActionMapper;
+import fr.carbonit.model.objects.Adventurer;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class GameEngine {
   @NonNull private final Game game;
 
   public void playGame() {
-    game.getAdventurerRegistry()
-        .forEach(
-            adventurer ->
-                adventurer.getMovements().stream()
-                    .map(EnumActionMapper::mapToAction)
-                    .forEach(abstractAction -> abstractAction.applyTo(game, adventurer)));
+    var liveAdventurers = getLiveAdventurers();
+    while (!liveAdventurers.isEmpty()) {
+      for (Adventurer a : liveAdventurers) {
+        var nextAction = a.getMovements().poll();
+        EnumActionMapper.mapToAction(
+                Optional.ofNullable(nextAction).orElseThrow(ShouldNotHappenException::new))
+            .applyTo(game, a);
+      }
+      liveAdventurers = getLiveAdventurers();
+    }
+  }
+
+  private List<Adventurer> getLiveAdventurers() {
+    return game.getAdventurerRegistry().stream()
+        .filter(adventurer -> !adventurer.getMovements().isEmpty())
+        .collect(Collectors.toList());
   }
 }
